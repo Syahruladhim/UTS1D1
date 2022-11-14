13e0c8298eeff08ad5240d96d7368efb8926f561
arul <syahruladhim101@gmail.com>
2022-11-14 13:23:59 +0700

    BELUMSELESAI1

--- src/JAWABANO3.java
+++ src/JAWABANO3.java
@@ -1,6 +1,12 @@
-public class JAWABANO3 {
-    public static void main(String[] args) {
-        System.out.println("halo sayang");
+public class JAWABANO3{
 
+    public static void ubahHurufETostar(String[] huruf){
+        for (int i=0; i<huruf.length;i++){
+            System.out.println(huruf[i].replace("e", "*"));
+        }
+    }
+    public static void main(String[] args) {
+        String[] Block = new String[]{"triangle", "cubes", "circle"};
+        ubahHurufETostar(Block);
     }
 }
