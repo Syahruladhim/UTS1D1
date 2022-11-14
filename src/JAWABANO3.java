87c17c0f529a63f9fcb80d2a34c31d9a916424a0
arul <syahruladhim101@gmail.com>
2022-11-14 19:08:43 +0700

    BELUMSELESAI1

--- src/JAWABANO3.java
+++ src/JAWABANO3.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
-public class tes {
-    public class tess{
+public class JAWABANO3 {
+    public class platnomor {
         static int jumlahPlat = 10;
-        static  int kudus = 0;
+        static int kudus = 0;
         static int jakarta = 0;
         static int jogja = 0;
         static int karawang = 0;
         static int solo = 0;
         static int sisaKuota = 0;
 
-        static  boolean success = false;
+        static boolean success = false;
 //       String[] InfoPlat = new String[]{"K-Kudus","B-Jakarta","AB-jogja","T-Karawang","AD-Solo"};
 
         public static void main(String[] args) {
 //           String[] InfoPlat = new String[]{"K-Kudus","B-Jakarta","AB-jogja","T-Karawang","AD-Solo"};
             for (int i = 0; 1 < 8; i++) {
-                infoplat();
+                platnomor();
             }
         }
+    }
+}
